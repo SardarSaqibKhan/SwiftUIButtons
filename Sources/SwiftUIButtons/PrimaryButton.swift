fc0ac4b4fea09c92925baace76d75749decce6c3
Sardar Saqib <44336686+SardarSaqibKhan@users.noreply.github.com>
2025-03-20 15:52:35 +0500

    done initial setup

---
+++ Sources/SwiftUIButtons/PrimaryButton.swift
@@ -0,0 +1,49 @@
+//
+//  PrimaryButton.swift
+//  
+//
+//  Created by sardar saqib on 20/03/2025.
+//
+
+import SwiftUI
+
+@available(iOS 13.0.0, *)
+public struct PrimaryButton: View {
+    var text: String
+    var fontSize: CGFloat
+    var backgroundColor: Color
+    var foregroundColor: Color
+    var cornerRadius: CGFloat
+    var action: () -> Void
+    
+    public init(text: String, fontSize: CGFloat, backgroundColor: Color, foregroundColor: Color, cornerRadius: CGFloat, action: @escaping () -> Void) {
+        self.text = text
+        self.fontSize = fontSize
+        self.backgroundColor = backgroundColor
+        self.foregroundColor = foregroundColor
+        self.cornerRadius = cornerRadius
+        self.action = action
+    }
+   
+    
+    public var body: some View {
+        Button(action: action) {
+            Text(text)
+                .foregroundColor(foregroundColor)
+                .padding()
+                .background(backgroundColor)
+                .cornerRadius(cornerRadius)
+        }
+    }
+}
+@available(iOS 13.0.0, *)
+#Preview {
+    PrimaryButton(
+        text: "Test Button",
+        fontSize: 17,
+        backgroundColor: .blue,
+        foregroundColor: .black,
+        cornerRadius: 10) {
+            
+        }
+}
--- Sources/SwiftUIButtons/SwiftUIButtons.swift
+++ Sources/SwiftUIButtons/SwiftUIButtons.swift
@@ -1,2 +1,26 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import SwiftUI
+
+@available(iOS 13.0.0, *)
+public struct SwiftUIButtons {
+    
+    @MainActor public static func primaryButton(
+        text: String,
+        fontsize: CGFloat = 17,
+        bgColor: Color = .blue,
+        fgColor: Color = .white,
+        cornerRadius: CGFloat = 10,
+        action: @escaping () -> Void
+    ) -> some View {
+        PrimaryButton(
+            text: text,
+            fontSize: fontsize,
+            backgroundColor: bgColor,
+            foregroundColor: fgColor,
+            cornerRadius: cornerRadius,
+            action: action
+        )
+    }
+}
